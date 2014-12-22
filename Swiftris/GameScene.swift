8f154346a4bb58fab81491aab3b0c9124155a434
Emiliano <emi@Asterix.local>
2014-12-21 23:16:42 -0300

    ticking update

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,7 +8,20 @@
 
 import SpriteKit
 
+///Constante que define la velocidad del juego en el primer nivel (en milisegundos). Cada este tiempo las figuras van a bajar un piso
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+    var tick:(() -> ())?
+    var tickLenghtMillis = TickLengthLevelOne
+
+    
+    ///Representa el momento de la ultima vez que se computo un frame (un tick)
+    ///
+    ///Si es nil, no se ejecuta el update()
+    var lastTick:NSDate?
+    
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -16,7 +29,11 @@ class GameScene: SKScene {
 
     override init(size: CGSize) {
         super.init(size: size)
+        
+        //La vista se ancla arriba a la izquierda
         anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        //Pinta el fondo y lo ancla arriba a la izquierda
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
@@ -25,7 +42,30 @@ class GameScene: SKScene {
 
     
     override func update(currentTime: CFTimeInterval) {
+        if lastTick == nil {
+            return
+        }
         
+        //Computa cuanto tiempo paso desde el ultimo tick 
+        //timeIntervalSinceNow calcula automaticamente la diferencia como un double. Ej: 0.857 son 857 milesimas (de ahi que se multiplica por 1000)
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        
+        //Si el tiempo que paso es mayor a un tick (600 ms en este caso), se llama al closure tick() y se resetea el contador lastTick a now
+        if timePassed > tickLenghtMillis {
+            lastTick = NSDate()
+            //El ? hace que solo se invoque a tick si este no es nulo
+            tick?()
+        }
+        
+    }
+    
+   
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
     
     
